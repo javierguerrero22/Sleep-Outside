58b17d22ad8b031706f90bf235176d723ddfd51b
javierguerrero22 <javierguerrero22jg@gmail.com>
2026-05-11 22:05:48 -0400

    r?

--- src/js/product.js
+++ src/js/product.js
@@ -4,12 +4,15 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  let cartItems = getLocalStorage("so-cart");
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
-  }
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
   cartItems.push(product);
   setLocalStorage("so-cart", cartItems);
+  // let cartItems = getLocalStorage("so-cart");
+  // if (!Array.isArray(cartItems)) {
+  //   cartItems = [];
+  // }
+  // cartItems.push(product);
+  // setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
